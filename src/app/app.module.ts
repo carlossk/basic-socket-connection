22695e9eecfab336476e63076533956ddec581cc
carlos sk <skyrko.carlos.sk@gmail.com>
2018-12-05 23:56:49 -0600

    init status connection socket

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { environment } from '../environments/environment';
+
+const config: SocketIoConfig = { url: environment.wsUrl, options: {} };
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/websocket.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {Socket} from 'ngx-socket-io';
+@Injectable({
+  providedIn: 'root'
+})
+export class WebsocketService {
+
+  public socketStatus = false;
+  constructor(private socket: Socket) {
+
+    this.checkStatus();
+   }
+
+  checkStatus() {
+
+    this.socket.on('connect', () => {
+
+      console.log('server connection');
+      this.socketStatus = true;
+    });
+    this.socket.on('disconnect', () => {
+
+      console.log('server disconnected');
+      this.socketStatus = false;
+    });
+  }
+}
